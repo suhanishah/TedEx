dd8dbbe9e3feae6654a47c9cdb590c38abe3475c
Suhani Shah <suhanishah@live.com>
2018-04-02 18:03:37 +0000

    InitialCommit

---
+++ TedEx/TedEx/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TedEx.MvcApplication" Language="C#" %>
---
+++ TedEx/TedEx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TedEx.Startup))]
+namespace TedEx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
