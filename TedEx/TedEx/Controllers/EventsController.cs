1328015b993c78d281bd12bcae5edcf6dc40da08
Suhani Shah <suhanishah@live.com>
2018-04-03 00:28:25 +0000

    Created Add a Topic Form

---
+++ TedEx/TedEx/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Linq;
+using System.Web.Mvc;
+using TedEx.Models;
+using TedEx.ViewModels;
+
+namespace TedEx.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel
+            {
+                Topics = _context.Topics.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ TedEx/TedEx/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TedEx.Models;
+
+namespace TedEx.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Topic { get; set; }
+        public IEnumerable<Topic> Topics { get; set; }
+    }
+}
